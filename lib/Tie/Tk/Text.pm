f5cc63a9741d2add7eee9e9e79dc55a76edd92cf
Michael Carman <mjcarman@cpan.org>
2006-12-21 17:42:53 +0000

    provides all methods for tied arrays (note: EXTEND is a no-op)

--- lib/Tie/Tk/Text.pm
+++ lib/Tie/Tk/Text.pm
@@ -1,63 +1,165 @@
 #===============================================================================
 # Tie/Tk/Text.pm
-# Last Modified: 11/19/2006 2:25PM
+# Last Modified: 11/21/2006 10:55AM
 #===============================================================================
 package Tie::Tk::Text;
 use strict;
 
 use vars qw'$VERSION';
-$VERSION = '0.01';
+$VERSION = '0.90';
 
-#-------------------------------------------------------------------------------
-# Method  : TIEARRAY
-# Purpose : Tie an array to a Text widget.
-# Notes   : 
-#-------------------------------------------------------------------------------
+# Notes:
+# * Text widgets use 1-based indexing so line number = array index + 1
+# * Text widgets *always* have a newline at the end. It's returned by
+#   $w->get('1.0', 'end') but isn't deleted by $w->delete('1.0', 'end')
+# * $w->get('1.0', '1.0 lineend') doesn't include the trailing newline.
+#   You need to use one of these instead:
+#       $w->get('1.0', '2.0')
+#       $w->get('1.0', '1.0 lineend + 1 chars)
+
 sub TIEARRAY {
 	my $class  = shift;
 	my $widget = shift;
-
 	return bless \$widget, $class;
 }
 
-#-------------------------------------------------------------------------------
-# Method  : FETCH
-# Purpose : Retrieve a line of text from a Text widget.
-# Notes   : Text widgets use 1-based indexing for line numbers
-#-------------------------------------------------------------------------------
+
+sub CLEAR {
+	my $self = shift;
+	$$self->delete('1.0', 'end');
+}
+
+
 sub FETCH {
 	my $self = shift;
 	my $line = shift() + 1;
-
 	return $$self->get("$line.0", "$line.end + 1 chars");
 }
 
-#-------------------------------------------------------------------------------
-# Method  : FETCHSIZE
-# Purpose : Retrieve the number of lines in Text widget
-# Notes   : Text widgets use 1-based indexing for line numbers
-#-------------------------------------------------------------------------------
-sub FETCHSIZE {
-	my $self = shift;
-	my ($line, $char) = split(/\./, $$self->index('end'));
 
-	# we need to account for the last line containing a newline, but I'm 
-	# not sure why we need to look back two chars instead of one.
-	my $lc = $$self->get('end - 2 chars');
-	return $lc eq "\n" ? $line - 2 : $line - 1;
+sub STORE {
+	my $self = shift;
+	my $idx  = shift;
+	my $text = shift;
+	my $line = $idx + 1;
+
+	$$self->insert('end', "\n")	while ($self->FETCHSIZE <= $idx);
+	$self->_delete($idx);
+	$$self->insert("$line.0", $text);
 }
 
-#sub STORE     {}
-#sub STORESIZE {}
-#sub EXTEND    {}
-#sub EXISTS    {}
-#sub DELETE    {}
-#sub CLEAR     {}
-#sub PUSH      {}
-#sub POP       {}
-#sub SHIFT     {}
-#sub UNSHIFT   {}
-#sub SPLICE    {}
+
+sub FETCHSIZE {
+	my $self = shift;
+	my $c    = $$self->get('end - 1 chars');
+	my $n    = $c eq "\n" ? 2 : 1; # cf. module notes
+	my $l    = (split(/\./, $$self->index("end - $n chars")))[0];
+	return $l;
+}
+
+
+sub STORESIZE {
+	my $self = shift;
+	my $size = shift;
+
+	if ($self->FETCHSIZE > $size) {
+		my $n = $size + 1;
+		$$self->delete("$n.0", 'end');
+	}
+	else {
+		$$self->insert('end', "\n") while ($self->FETCHSIZE < $size);
+	}
+}
+
+
+sub EXISTS {
+	my $self = shift;
+	my $idx  = shift;
+	return $idx < $self->FETCHSIZE;
+}
+
+
+sub DELETE {
+	my $self = shift;
+	my $idx  = shift;
+	my $text = $self->EXISTS($idx) ? $self->FETCH($idx) : undef;
+
+	my $l = $idx+1;
+	$$self->delete("$l.0", "$l.0 lineend"); # don't delete the \n
+
+	# collapse trailing "undef" (\n) values
+	while ($self->FETCH($self->FETCHSIZE - 1) eq "\n") {
+		$self->POP;
+	}
+	
+	return $text;
+}
+
+
+sub PUSH {
+	my $self = shift;
+	$$self->insert('end', $_) foreach @_;
+}
+
+
+sub POP {
+	my $self = shift;
+	return $self->_delete($self->FETCHSIZE - 1);
+}
+
+
+sub UNSHIFT {
+	my $self = shift;
+	$$self->insert('1.0', $_) foreach reverse @_;
+}
+
+
+sub SHIFT {
+	my $self = shift;
+	return $self->_delete(0);
+}
+
+
+sub SPLICE {
+	my $self = shift;
+	my $o    = shift; # offset
+	my $l    = shift; # length
+	
+	$o = 0                          unless defined $o;
+	$o = $self->FETCHSIZE + $o      if     $o < 0;
+	$l = $self->FETCHSIZE - $o      unless defined $l;
+	$l = $self->FETCHSIZE - $o + $l if     $l < 0;
+
+	my @deleted;
+	foreach my $i (reverse ($o .. $o + $l - 1)) {
+		unshift @deleted, $self->_delete($i);
+	}
+
+	foreach my $r (reverse @_) {
+		my $x = $o + $l - 1;
+		$$self->insert("$x.0", $r);
+	}
+
+	return @deleted;
+}
+
+
+sub EXTEND {}
+
+#-------------------------------------------------------------------------------
+# Method  : Remove and (really) delete element
+# Purpose : 
+# Notes   : 
+#-------------------------------------------------------------------------------
+sub _delete {
+	my $self = shift;
+	my $idx  = shift;
+	my $text = $self->FETCH($idx);
+	my $line = $idx+1;
+
+	$$self->delete("$line.0", "$line.end + 1 chars");
+	return $text;
+}
 
 1;
 
@@ -82,16 +184,31 @@ Tie::Tk::Text - Access Tk::Text or Tk::ROText widgets as arrays.
 
 =head1 DESCRIPTION
 
-This module defines a class for tie()ing Tk::Text and Tk::ROText widgets to an
+This module defines a class for tie()ing Tk::Text and Tk::ROText widgets to an 
 array, allowing them to be accessed as if they were an array of lines.
 
-It's not expected that anyone will actually want to populate their Text widgets
-this way. Instead, this makes them accessible to functions which expect an
-array reference as their input. (e.g. Algorithm::Diff::sdiff)
+It's not expected that anyone will actually want to populate and manipulate 
+their Text widgets this way, though you are of course free to do so. This module 
+was created to make Text widgets accessible to functions that expect an array 
+reference as their input. (e.g. Algorithm::Diff::sdiff) You can do that with 
+read-only support (FETCH and FETCHSIZE). All of the methods (PUSH, POP, STORE, 
+etc.) are included for completeness.
 
-=head1 BUGS
+=head1 CAVEATS
 
-Only provides read support at present. (i.e. FETCH and FETCHSIZE)
+Arrays use zero-based indexing. Text widgets use one-based indexing. Ergo, line 
+five is at index four.
+
+Lines end in "\n". Be careful about what you add or you could get odd results. 
+For example, doing C<$text[3] = 'foo'> will replace the contents of line four 
+with 'foo' but will join lines four and five because you didn't include a 
+newline at the end of the string. Similarly, C<$text[3] = "foo\nbar\n"> will 
+replace line four with "foo\n" and B<insert> "bar\n" before line five.
+
+=head1 LIMITATIONS
+
+There's no support for tags or a lot of other things that you can do with Text 
+widgets. There isn't supposed to be. This is not a bug.
 
 =head1 AUTHOR
 
--- t/Tie-Tk-Text.t
+++ t/Tie-Tk-Text.t
@@ -1,15 +1,122 @@
+#===============================================================================
+# Tie-Tk-Text.t
+# Last Modified: 11/21/2006 10:54AM
+#===============================================================================
+# Test suite for Tie::Tk::Text
+#===============================================================================
 use Tk;
-use Test::More tests => 4;
+use Test::More tests => 40;
 
 BEGIN { use_ok('Tie::Tk::Text') };
 
 my $mw = MainWindow->new();
-my $w  = $mw->Text();
+my $w  = $mw->Text()->pack();
 my @text;
 
 tie @text, 'Tie::Tk::Text', $w;
 is(${tied(@text)}, $w, 'TIEARRAY');
 
+
+# FETCH/STORE, FETCHSIZE, CLEAR
 $w->insert('end', "foo\nbar\nbaz\n");
+
 is(@text, 3, 'FETCHSIZE');
+
 is($text[1], "bar\n", 'FETCH');
+
+my $z = $text[1] = "BAR\n";
+is($w->get('2.0', '2.0 lineend + 1 chars'), "BAR\n", 'STORE');
+
+$text[5] = "woo\n";
+is($w->get('4.0', '6.0 lineend + 1 chars'), "\n\nwoo\n", 'STORE extends');
+
+@text = ();
+is($w->get('1.0', 'end'), "\n", 'CLEAR [@a = ()]');
+
+$w->insert('end', "foo\nbar\nbaz\n");
+undef @text;
+is($w->get('1.0', 'end'), "\n", 'CLEAR [undef @a]');
+
+
+# PUSH/POP, SHIFT/UNSHIFT
+$w->delete('1.0', 'end');
+$w->insert('1.0', "three\n");
+
+unshift @text, "one\n", "two\n";
+is($w->get('1.0', 'end'), "one\ntwo\nthree\n\n", 'UNSHIFT');
+
+push @text, "four\n", "five\n";
+is($w->get('1.0', 'end'), "one\ntwo\nthree\nfour\nfive\n\n", 'PUSH');
+
+is(shift @text, "one\n", 'SHIFT returns line');
+is(      @text, 4,       'SHIFT removes line');
+
+is(pop @text, "five\n", 'POP returns line');
+is(    @text, 3,        'POP removes line');
+
+
+# EXISTS/DELETE
+$w->delete('1.0', 'end');
+$w->insert('1.0', "1\n2\n3\n4\n5\n6\n7\n");
+
+ok( exists $text[2], 'EXISTS (in range)');
+ok(!exists $text[7], 'EXISTS (at range boundary)');
+ok(!exists $text[8], 'EXISTS (out of range)');
+
+is(delete($text[2]),            "3\n",                    'DELETE $a[x] returns deleted');
+is($w->get('1.0', 'end'),       "1\n2\n\n4\n5\n6\n7\n\n", 'DELETE $a[x] clears line');
+is(delete(@text[3..4]),         "5\n",                    'DELETE @a[x..y] returns list (not array) in scalar context');
+is($w->get('1.0', 'end'),       "1\n2\n\n\n\n6\n7\n\n",   'DELETE @a[x..y] clears lines');
+is_deeply([delete @text[5..6]], ["6\n", "7\n"],           'DELETE @a[x..y] returns list in list context');
+is($w->get('1.0', 'end'),       "1\n2\n\n",               'DELETE shrinks array');
+
+
+# STORESIZE
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n");
+
+$#text = 3;
+is(@text, 4, 'STORESIZE deletes');
+
+$#text = 4;
+is(@text, 5, 'STORESIZE extends');
+
+$#text = 3;
+is(@text, 4, 'STORESIZE deletes');
+
+
+# SPLICE
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n");
+is_deeply([splice(@text)], ["1\n", "2\n", "3\n", "4\n", "5\n"], 'SPLICE(ARRAY) return list');
+is($w->get('1.0', 'end'),  "\n",                                'SPLICE(ARRAY) remainder');
+
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n");
+is_deeply([splice(@text, 2)], ["3\n", "4\n", "5\n"], 'SPLICE(ARRAY, OFFSET) return list');
+is($w->get('1.0', 'end'),     "1\n2\n\n",            'SPLICE(ARRAY, OFFSET) remainder');
+
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n");
+is_deeply([splice(@text, 2, 2)], ["3\n", "4\n"], 'SPLICE(ARRAY, OFFSET, LENGTH) return list');
+is($w->get('1.0', 'end'),        "1\n2\n5\n\n",  'SPLICE(ARRAY, OFFSET, LENGTH) remainder');
+
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n");
+is_deeply([splice(@text, 2, 2, "A\n", "B\n", "C\n")], ["3\n", "4\n"],         'SPLICE(ARRAY, OFFSET, LENGTH, LIST) return list');
+is($w->get('1.0', 'end'),                             "1\n2\nA\nB\nC\n5\n\n", 'SPLICE(ARRAY, OFFSET, LENGTH, LIST) remainder');
+
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n");
+is_deeply([splice(@text, -2)], ["4\n", "5\n"], 'SPLICE(ARRAY, -OFFSET) return list');
+is($w->get('1.0', 'end'),     "1\n2\n3\n\n",   'SPLICE(ARRAY, -OFFSET) remainder');
+
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n6\n");
+is_deeply([splice(@text, 1, -2)], ["2\n", "3\n", "4\n"], 'SPLICE(ARRAY, OFFSET, -LENGTH) return list');
+is($w->get('1.0', 'end'),         "1\n5\n6\n\n",         'SPLICE(ARRAY, OFFSET, -LENGTH) remainder');
+
+$w->delete('1.0', 'end');
+$w->insert('end', "1\n2\n3\n4\n5\n6\n");
+is_deeply([splice(@text, -5, -2)], ["2\n", "3\n", "4\n"], 'SPLICE(ARRAY, -OFFSET, -LENGTH) return list');
+is($w->get('1.0', 'end'),          "1\n5\n6\n\n",         'SPLICE(ARRAY, -OFFSET, -LENGTH) remainder');
